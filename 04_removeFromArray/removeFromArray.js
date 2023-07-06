42d27702d80664280bae75735fe82e09777bf5dc
Natalia Aspee <n.aspeedelgado@gmail.com>
2023-07-06 18:37:55 -0400

    solution to removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+// const removeFromArray = function (array, deleted) {
+//     //hacemos un filter para que elimine el elemento deleted del array, funciona pero con un solo argumento
+//     const result = array.filter(arr => arr != deleted);
+//     return result;
+// };
 
+const removeFromArray = function (array, ...args) {
+    //hacemos un filter para que elimine el elemento deleted del array, funciona pero con un solo argumento
+    const result = array.filter(arr => !args.includes(arr));
+    return result;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
