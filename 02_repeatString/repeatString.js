c3166d83444a1582b696112905c7bfc9e8d1298c
Natalia Aspee <n.aspeedelgado@gmail.com>
2023-07-06 17:21:25 -0400

    add repeat string solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+// return string.repeat(num)
+    //declaramos let vacio para el string repetido
+    let resultRepeat = "";
+    //returns ERROR with negative numbers
+    if (num <0) {return "ERROR"}
+    //ciclo for para repetir el string segun el num
+    for (let i = 0; i<num; i++){
+        resultRepeat += string;
+    }
+      return resultRepeat  
 };
-
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
